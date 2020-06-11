c84673eb18943414ea07e97773ded7f3727df6fe
Maxime Britto <maxime@purplegiraffe.fr>
2020-06-11 08:51:54 +0200

    Changed the format to a formal library

--- BriskScript/Package.swift
+++ BriskScript/Package.swift
@@ -1,15 +1,25 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
-    name: "BriskScript",
-    platforms: [
-        .macOS(.v10_15)
+    name: "Brisk",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "Brisk",
+            targets: ["Brisk"]),
     ],
     dependencies: [
+        // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        .target(name: "BriskScript", dependencies: [], path: "." )
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "Brisk",
+            dependencies: []),
     ]
 )
